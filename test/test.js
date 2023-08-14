1de170cc3199d0562b1e909fe32189d63131b406
parteek kumar <digitalparteek@gmail.com>
2023-08-14 14:16:21 +0530

    prod-test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const {expect, describe, it} = require("chai");
+const {JSDOM} = require("jsdom")
+
+describe('Website', () => {
+    it('should have a header element', () => {
+      const dom = new JSDOM('<!DOCTYPE html><html><head></head><body><header></header></body></html>');
+      const header = dom.window.document.querySelector('header');
+      expect(header).to.exist;
+    });
+  });
